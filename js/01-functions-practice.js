98cdf533ffc4b3b869fcad40697466cd3bd8d1b8
dahwindelacruz <dahwin.delacruz@gmail.com>
2020-02-20 21:02:31 -0800

    update

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -1,9 +1,24 @@
 //STEP 1
+// function halfNumber (number) {
+//     window.console.log("Half of " + number + " is " + number / 2 + ".");
+// }
+// halfNumber(10);
 
 //STEP 2
+// function squareNumber (number) {
+//     window.console.log("The result of squaring the number " + number + " is " + number * number + ".")
+// }
+// squareNumber(3);
 
 //STEP 3
+// function percentOf (number1, number2) {
+//     window.console.log(number1 + " is " + parseFloat(number1 / number2 * 100).toFixed(1) + "% of " + number2 + ".");
+// }
+// percentOf(2, 4);
 
 //STEP 4
+function findModulus () {
+    
+}
 
 //STEP 5
