783794771bcc470f00b158099f3fe0456a3fafb5
dahwindelacruz <dahwin.delacruz@gmail.com>
2020-02-26 17:04:01 -0800

    update

---
+++ js/02-fortune-teller.js
@@ -0,0 +1,7 @@
+function tellFortune (numberofchildren, partnerName, geographicLocation, jobTitle) {
+    document.write("You will be a " + jobTitle + " in " + geographicLocation + ", and married to " + partnerName + " with " + numberofchildren + " kids.</br>")
+}
+tellFortune (4, "Jennie", "Hawaii", "F1 Racing Driver");
+tellFortune (3, "Jisoo", "Seoul", "Video Game Developer");
+tellFortune (2, "Rose", "Toronto", "Professional Baseball Player");
+tellFortune (1, "Lisa", "San Francisco", "Dance Choreographer");
