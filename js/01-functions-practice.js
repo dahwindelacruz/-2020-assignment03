e8e11422ff016bb2cf5c0bc55b339787c588756f
dahwindelacruz <dahwin.delacruz@gmail.com>
2020-02-26 16:39:46 -0800

    update

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -24,3 +24,10 @@
 // findModulus(4, 10);
 
 //STEP 5
+function findSum () {
+    number1 = parseInt(window.prompt("Enter a number"));
+    number2 = parseInt(window.prompt("Enter another number"));
+    number3 = parseInt(window.prompt("Enter one more number"));
+    window.console.log("The sum is " + (number1 + number2 + number3) + ".");
+}
+findSum();
